e664a8c0bf57a98e26660dceeae7c8738f5fd659
Marc <marcstruchmalonda@gmail.com>
2025-11-12 20:56:32 +0100

    Iniciando necesidades login.php

--- login.php
+++ login.php
@@ -1,4 +1,26 @@
 <?php
+session_start();
+include_once __DIR__.'/models/Usuari.php';
+include_once __DIR__.'/models/UsuariDAO.php';
+include_once __DIR__.'/models/utils.php';
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  //Limpiar datos recibidos
+  $email= neteja_dades($_POST['email']);
+  $pass= neteja_dades($_POST['pass1']);
+  //verificar que no esten vacios los campos
+  if(empty($email) || empty($pass)){
+    $_SESSION['misssatge_error']= "Te falta algun campo por rellenar porfavor intentalo otra vez entrando a registrarse";
+    header("Location: index.php");
+    exit;
+  }
+  //verificar si no existe email
+  $hayEmail = UsuariDAO::selectByMail($email);
+  if(!$hayEmail){
+    $_SESSION['misssatge_error']= "El usuario con email:".$email." no existe.";
+    header("Location: index.php");
+    exit;
+  }
+}
 include_once __DIR__ . '/header.php';
 ?>
 
@@ -6,6 +28,15 @@ include_once __DIR__ . '/header.php';
     <div class="card" style="width: 24rem;">
       <div class="card-body">
         <h5 class="card-title text-center mb-4">Iniciar Sessió</h5>
+        <?php
+          if(!empty($missatge_error)):
+        ?>
+          <div class="alert alert-danger" role="alert">
+            <?= $missatge_error?>
+          </div>
+        <?php
+          endif;
+        ?>
         <form action="#" method="post">
           <!-- Usuari -->
           <div class="mb-3">
