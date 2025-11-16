93235e661ebe733da3cefe517a19bef1e827f76a
Marc <marcstruchmalonda@gmail.com>
2025-11-16 14:33:57 +0100

    Funcion selectByUserPeliId añadida en ValoracioDAO

--- models/ValoracioDAO.php
+++ models/ValoracioDAO.php
@@ -4,6 +4,23 @@ require_once __DIR__. '/DBConnection.php';
 require_once __DIR__. '/IDbAccess.php';
 
 class ValoracioDAO{
-    
+    public static function selectByUserPeliId($usuari_id, $peli_id): ?Valoracio
+    {
+        $conn= DBConnection::connectDB();
+        if(!is_null($conn)){
+            $stmt = $conn->prepare("SELECT id, peli_id, usuari_id, valoracio
+                                    FROM valoracions
+                                    WHERE usuari_id = :usuari_id
+                                    AND peli_id = :peli_id");
+            $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Valoracio');
+            $stmt->execute(['usuari_id'=>$usuari_id,
+                            'peli_id'=>$peli_id]);
+            $valoracio = $stmt->fetch(); 
+            if($valoracio){
+                return $valoracio;
+            }    
+        }
+        return null;
+    }
 }
 ?>
